d969258a6dcb98fdfc53cd010bdcc4afcaec9753
Egor Piskunou <egor.piskunou@gmail.com>
2018-01-13 02:23:22 +0300

    add few new colors to Colors.js

--- variables/Colors.js
+++ variables/Colors.js
@@ -2,19 +2,25 @@ export default {
   // Link to color scheme:
   // https://colorscheme.ru/#3E51TpwLAw0w0
 
-  // Primary Color
+  // Primary Colors
   primaryColor: '#3A7BD5',
   primaryColorDark: '#13458A',
   primaryColorLight: '#8CB4EA',
 
-  // Secondary Color
+  // Secondary Colors
   secondaryColor: '#5743D9',
   secondaryColorDark: '#26168D',
   secondaryColorLight: '#9E92EC',
 
-  // Accent Color
+  // Accent Colors
   accentColor: '#2BD3AE',
   accentColorDark: '#0E896E',
-  accentColorLight: '#83E9D2'
+  accentColorLight: '#83E9D2',
+
+  // Informing Colors
+  infoColor: '#62B1F6',
+  successColor: '#5cb85c',
+  dangerColor: '#d9534f',
+  warningColor: '#f0ad4e',
 
 };
