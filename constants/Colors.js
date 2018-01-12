ca76a37cd114ca43900ddbb9b0c2c0bb440a17d9
Egor Piskunou <egor.piskunou@gmail.com>
2018-01-13 01:50:26 +0300

    add Color constant
    lint to color scheme https://colorscheme.ru/#3E51TpwLAw0w0

---
+++ constants/Colors.js
@@ -0,0 +1,20 @@
+export default {
+  // Link to color scheme:
+  // https://colorscheme.ru/#3E51TpwLAw0w0
+
+  // Primary Color
+  primaryColor: '#3A7BD5',
+  primaryColorDark: '#13458A',
+  primaryColorLight: '#8CB4EA',
+
+  // Secondary Color
+  secondaryColor: '#5743D9',
+  secondaryColorDark: '#26168D',
+  secondaryColorLight: '#9E92EC',
+
+  // Accent Color
+  accentColor: '#2BD3AE',
+  accentColorDark: '#0E896E',
+  accentColorLight: '#83E9D2'
+
+};
