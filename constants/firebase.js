9c85e226217a9894b4ddca2c51ef331991a6bead
Egor Piskunou <egor.piskunou@gmail.com>
2018-01-12 22:10:26 +0300

    add firebase configurations

---
+++ constants/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyB_NRft-kpgEZvov_F6LLJDHFoVzXL9Xk0",
+  authDomain: "keepcoin-b2e56.firebaseapp.com",
+  databaseURL: "https://keepcoin-b2e56.firebaseio.com",
+  projectId: "keepcoin-b2e56",
+  storageBucket: "keepcoin-b2e56.appspot.com",
+  messagingSenderId: "795848223835"
+};
