12a733fefe8f564f9dd02eabc3b85c1fe1ed5027
Egor Piskunou <egor.piskunou@gmail.com>
2018-01-12 23:16:28 +0300

    add const of errors

---
+++ constants/errors.js
@@ -0,0 +1,17 @@
+export default {
+  // Default
+  default: 'Неизвестная ошибка',
+  timeout: 'Проверьте ваше интернет подключение. Server Timed Out',
+  invalidJSON: 'Получен не верный ответ от сервера',
+
+  // Firebase
+  invalidFirebase: 'Ошибка подключение к Firebase',
+
+
+  // User
+  userExists: 'Такой пользователь уже существует',
+  missingEmail: 'Вы не ввели email',
+  missingPassword: 'Вы не ввели пароль',
+  passwordDontMatch: 'Пароли не совпадают'
+
+};
