0d77f1182634137c9846b9c01361ead0c9e780bd
Egor Piskunou <egor.piskunou@gmail.com>
2018-01-13 02:22:31 +0300

    Revert "add few new colors to Colors.js"
    This reverts commit 66706002d89dd8647c7969888f45f20baf28f00f.

--- variables/Colors.js
+++ variables/Colors.js
@@ -2,25 +2,19 @@ export default {
   // Link to color scheme:
   // https://colorscheme.ru/#3E51TpwLAw0w0
 
-  // Primary Colors
+  // Primary Color
   primaryColor: '#3A7BD5',
   primaryColorDark: '#13458A',
   primaryColorLight: '#8CB4EA',
 
-  // Secondary Colors
+  // Secondary Color
   secondaryColor: '#5743D9',
   secondaryColorDark: '#26168D',
   secondaryColorLight: '#9E92EC',
 
-  // Accent Colors
+  // Accent Color
   accentColor: '#2BD3AE',
   accentColorDark: '#0E896E',
-  accentColorLight: '#83E9D2',
-
-  // Informing Colors
-  infoColor: '#62B1F6',
-  brandSuccessColor: '#5cb85c',
-  brandDangerColor: '#d9534f',
-  brandWarningColor: '#f0ad4e',
+  accentColorLight: '#83E9D2'
 
 };
