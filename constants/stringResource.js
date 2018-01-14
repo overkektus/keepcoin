fc7c48ebdea6064e87e00939ea62e152d0717c45
Egor Piskunou <egor.piskunou@gmail.com>
2018-01-14 15:54:40 +0300

    add new string resource

--- constants/stringResource.js
+++ constants/stringResource.js
@@ -1,4 +1,14 @@
 export default {
+  // Authentication, Registration and ResePassword
   authText: 'Авторизация',
-  regText: 'Регистрация'
+  regText: 'Регистрация',
+  regActionText: 'Зарегистрироваться',
+  resetPasswordText: 'Забыли пароль?',
+  resetPasswordActionText: 'Восстановить пароль',
+
+  
+  emailText: 'Email',
+  passwordText: 'Пароль',
+  usernameText: 'Логин',
+  confirmPasswordText: 'Повторите пароль'
 }
